c9f4ba70578172e0459eb0c95163ff59d95ffab1
Brian Hou <brian.hou@live.com>
2022-10-16 13:33:18 -0400

    Update target Python versions.

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-![Build Status](https://github.com/brianhou/bombast/workflows/tests/badge.svg)
+# bombast ![Build Status](https://github.com/brianhou/bombast/workflows/tests/badge.svg) [![Python 3.8+](https://img.shields.io/badge/python-3.8+-blue.svg)](https://www.python.org/downloads/release/python-380/)
 
-bombast
-=======
-
-An obfuscator for Python source code that manipulates the AST.
+An obfuscator for Python 3 source code that manipulates the AST.
 
 Adapted from http://jbremer.org/wp-posts/hitbctfobf.py.
--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='bombast',
-    version='v0.2.4',
-    description='An obfuscator for Python source code that manipulates the AST.',
-    url='https://github.com/brianhou/bombast',
-    author='Brian Hou',
-    packages=['bombast'],
-    install_requires=['astunparse'],
+    name="bombast",
+    version="v0.3.0",
+    description="An obfuscator for Python 3 source code that manipulates the AST.",
+    url="https://github.com/brianhou/bombast",
+    author="Brian Hou",
+    packages=["bombast"],
+    install_requires=["astunparse>=1.6.3"],
+    python_requires="~=3.8",
     entry_points={
-        'console_scripts': [
-            'bombast=bombast.__init__:main',
+        "console_scripts": [
+            "bombast=bombast.__init__:main",
         ],
-    }
+    },
 )
