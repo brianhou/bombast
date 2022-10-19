026dafd33f927b4afc0b1db3ea215c1acb2bee8a
Brian Hou <brianhou@users.noreply.github.com>
2022-10-18 20:21:20 -0400

    Update setup.py. (#13)

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="bombast",
     version="v0.3.0",
     description="An obfuscator for Python 3 source code that manipulates the AST.",
     url="https://github.com/brianhou/bombast",
     author="Brian Hou",
-    packages=["bombast"],
+    license="MIT",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=setuptools.find_packages(include=["bombast", "bombast.*"]),
     python_requires="~=3.9",
     entry_points={
         "console_scripts": [
